30e728843b34a5301dbaff60a88d87e24cf79ba2
Kenny <88187389+blasiankenny@users.noreply.github.com>
2023-05-03 21:50:55 -0700

    Add files via upload

--- my-closet/scripts/closet_general.js
+++ my-closet/scripts/closet_general.js
@@ -1,14 +1,127 @@
-const slides = document.querySelectorAll(".slide");
-const nextBtn = document.querySelector(".nextBtn");
-const prevBtn = document.querySelector(".prevBtn");
+import { closet, mannequins } from './items_list.js';
+const newCloset = closet;
+const newMannequins = mannequins;
 
-document.querySelector(".num-items-button").innerHTML=
-  `1/${slides.length}`;
 
-slides.forEach(function (slide, index) {
-  slide.style.left = `${index * 100}%`;
+window.addEventListener('DOMContentLoaded', () => {
+  displayMannequin();
 });
-let counter = 0;
+
+document.querySelector(".jacket").addEventListener('click', () => {
+  displayClosetItems("jacket", "up");
+  makeSlides();
+})
+document.querySelector(".tops").addEventListener('click', () => {
+  displayClosetItems("tops", "up");
+  makeSlides();
+})
+document.querySelector(".bottoms").addEventListener('click', () => {
+  displayClosetItems("bottoms", "low");
+  makeSlidesLower();
+})
+
+const mannequinId = 1;
+const mannequinIdLower = 2;
+
+function displayMannequin() {
+
+  let mannequin;
+  let mannequinLower;
+
+  for (let i = 0; i < newMannequins.length; i++) {
+    const id = newMannequins[i].id;
+    if (id === mannequinId)
+      mannequin = newMannequins[i];
+
+    if (id === mannequinIdLower)
+      mannequinLower = newMannequins[i];
+  }
+
+  document.querySelector('.mannequin-upper').innerHTML = `
+  <img src=${mannequin.img} alt=${mannequin.title} class="photo">
+  `;
+  // document.querySelector('.mannequin-lower').innerHTML = `
+  // <img src=${mannequinLower.img} alt=${mannequinLower.title} class="photo">
+  // `;
+
+}
+
+function displayClosetItems(category, upOrLow = "up") {
+
+
+  let display;
+  if (upOrLow === "up") {
+    display = newCloset.map((item) => {
+      if (item.category === category) {
+        return `<div class="slide"><img class="slide-img ${item.title} ${item.category}" src="${item.img}" alt="" /></div>
+      `;
+      }
+    });
+  } else {
+    display = newCloset.map((item) => {
+      if (item.category === category) {
+        return `<div class="slide-lower"><img class="slide-img-lower ${item.title} ${item.category}" src="${item.img}" alt="" /></div>
+      `;
+      }
+    });
+  }
+  display = display.join("");
+  if (upOrLow === "up") {
+    document.querySelector('#fitting-room .slider-container .items').
+      innerHTML = display;
+  } else {
+    document.querySelector('#fitting-room-lower .slider-container-lower .items').
+      innerHTML = display;
+  }
+
+}
+
+// function displayMenuButtons() {
+//   let categories = newCloset.reduce((values, item) => {
+//     if (!values.includes(item.category)) {
+//       values.push(item.category);
+//     }
+//     return values;
+//   }, ['whole']);
+//   categories = ['jacket'];
+//   const categoryBtns = categories.map((category) => {
+//     return `<button class="filter-btn" type="button" data-id="${category}">${category}</button>`;
+//   }).join("");
+
+//   document.querySelector(".filter-buttons-container").innerHTML = categoryBtns;
+
+//   const filterBtns = document.querySelectorAll('.filter-btn');
+
+//   //filter items
+
+//   filterBtns.forEach((btn) => {
+//     btn.addEventListener('click', (e) => {
+//       const category = e.currentTarget.dataset.id;
+//       const itemCategory = newCloset.filter((closetItem) => {
+//         return closetItem.category === category;
+
+//       });
+//       if (category === 'whole') {
+//         displayClosetItems(newCloset);
+//       } else {
+//         displayClosetItems(itemCategory);
+//       }
+//   displayMenuButtons();
+//   makeSlides();
+//     })
+//   })
+
+
+// };
+
+
+let slides = document.querySelectorAll(".slide");
+let nextBtn = document.querySelector(".nextBtn");
+let prevBtn = document.querySelector(".prevBtn");
+
+
+let counter;
+
 nextBtn.addEventListener("click", function () {
   counter++;
   carousel();
@@ -19,6 +132,23 @@ prevBtn.addEventListener("click", function () {
   carousel();
 });
 
+function makeSlides() {
+  counter = 0;
+  slides = document.querySelectorAll(".slide");
+
+  nextBtn = document.querySelector(".nextBtn");
+  prevBtn = document.querySelector(".prevBtn");
+  document.querySelector(".num-items-button").innerHTML =
+    `1/${slides.length}`;
+
+  slides.forEach(function (slide, index) {
+    slide.style.left = `${index * 100}%`;
+  });
+
+  
+}
+
+
 function carousel() {
   // working with slides
   if (counter === slides.length) {
@@ -28,17 +158,17 @@ function carousel() {
     counter = slides.length - 1;
   }
 
-  document.querySelector(".num-items-button").innerHTML=
-  `${counter+1}/${slides.length}`;
+  document.querySelector(".num-items-button").innerHTML =
+    `${counter + 1}/${slides.length}`;
   // working with buttons
 
   if (counter < slides.length - 1) {
     nextBtn.style.display = "block";
-  } 
+  }
 
   if (counter > 0) {
     prevBtn.style.display = "block";
-  } 
+  }
   slides.forEach(function (slide) {
     slide.style.transform = `translateX(-${counter * 100}%)`;
   });
@@ -46,43 +176,31 @@ function carousel() {
 
 
 
-document.querySelector(".colourBtn").addEventListener("click", () => {
-    const container = document.querySelector(".slider-container");
+// document.querySelector(".colourBtn").addEventListener("click", () => {
+//     const container = document.querySelector(".slider-container");
 
-    const R=Math.floor(Math.random()*256);
-    const G=Math.floor(Math.random()*256);
-    const B=Math.floor(Math.random()*256);
+//     const R=Math.floor(Math.random()*256);
+//     const G=Math.floor(Math.random()*256);
+//     const B=Math.floor(Math.random()*256);
 
-    container.style.background=`rgb(${R},${G},${B})`;
-})
+//     container.style.background=`rgb(${R},${G},${B})`;
+// })
 
-document.querySelector(".resetBtn").addEventListener
-("click", ()=>{
-    document.querySelector(".slider-container").style.background=`rgb(255,255,255)`;
-})
-document.querySelector(".resetBtn-black").addEventListener
-("click", ()=>{
-    document.querySelector(".slider-container").style.background=`rgb(0,0,0)`;
-})
+// document.querySelector(".resetBtn").addEventListener
+// ("click", ()=>{
+//     document.querySelector(".slider-container").style.background=`rgb(255,255,255)`;
+// })
+// document.querySelector(".resetBtn-black").addEventListener
+// ("click", ()=>{
+//     document.querySelector(".slider-container").style.background=`rgb(0,0,0)`;
+// })
 
+let slides_lower = document.querySelectorAll(".slide-lower");
+let nextBtn_lower = document.querySelector("#fitting-room-lower .nextBtn");
+let prevBtn_lower = document.querySelector("#fitting-room-lower .prevBtn");
 
+let counter_lower;
 
-
-
-
-
-
-const slides_lower = document.querySelectorAll(".slide-lower");
-const nextBtn_lower = document.querySelector(".nextBtn-lower");
-const prevBtn_lower = document.querySelector(".prevBtn-lower");
-
-document.querySelector(".num-items-button-lower").innerHTML=
-  `1/${slides.length}`;
-
-slides_lower.forEach(function (slide, index) {
-  slide.style.left = `${index * 100}%`;
-});
-let counter_lower = 0;
 nextBtn_lower.addEventListener("click", function () {
   counter_lower++;
   carousel_lower();
@@ -93,6 +211,23 @@ prevBtn_lower.addEventListener("click", function () {
   carousel_lower();
 });
 
+function makeSlidesLower() {
+  counter_lower = 0;
+  
+  slides_lower = document.querySelectorAll(".slide-lower");
+
+  nextBtn = document.querySelector("#fitting-room-lower .nextBtn");
+  prevBtn = document.querySelector("#fitting-room-lower .prevBtn");
+  document.querySelector(".num-items-button-lower").innerHTML =
+    `1/${slides_lower.length}`;
+
+
+  slides_lower.forEach(function (slide, index) {
+    slide.style.left = `${index * 100}%`;
+  });
+  
+}
+
 function carousel_lower() {
   // working with slides
   if (counter_lower === slides_lower.length) {
@@ -102,17 +237,17 @@ function carousel_lower() {
     counter_lower = slides_lower.length - 1;
   }
 
-  document.querySelector(".num-items-button-lower").innerHTML=
-  `${counter_lower+1}/${slides_lower.length}`;
+  document.querySelector(".num-items-button-lower").innerHTML =
+    `${counter_lower + 1}/${slides_lower.length}`;
   // working with buttons
 
   if (counter_lower < slides_lower.length - 1) {
-    nextBtn.style.display = "block";
-  } 
+    nextBtn_lower.style.display = "block";
+  }
 
   if (counter_lower > 0) {
     prevBtn_lower.style.display = "block";
-  } 
+  }
   slides_lower.forEach(function (slide) {
     slide.style.transform = `translateX(-${counter_lower * 100}%)`;
   });
@@ -120,22 +255,22 @@ function carousel_lower() {
 
 
 
-document.querySelector(".colourBtn-lower").addEventListener("click", () => {
-    const container = document.querySelector(".slider-container-lower");
+// document.querySelector(".colourBtn-lower").addEventListener("click", () => {
+//     const container = document.querySelector(".slider-container-lower");
 
-    const R=Math.floor(Math.random()*256);
-    const G=Math.floor(Math.random()*256);
-    const B=Math.floor(Math.random()*256);
+//     const R=Math.floor(Math.random()*256);
+//     const G=Math.floor(Math.random()*256);
+//     const B=Math.floor(Math.random()*256);
 
-    container.style.background=`rgb(${R},${G},${B})`;
-})
+//     container.style.background=`rgb(${R},${G},${B})`;
+// })
 
-document.querySelector(".resetBtn-lower").addEventListener
-("click", ()=>{
-    document.querySelector(".slider-container-lower").style.background=`rgb(255,255,255)`;
-})
-document.querySelector(".resetBtn-black-lower").addEventListener
-("click", ()=>{
-    document.querySelector(".slider-container-lower").style.background=`rgb(0,0,0)`;
-})
+// document.querySelector(".resetBtn-lower").addEventListener
+// ("click", ()=>{
+//     document.querySelector(".slider-container-lower").style.background=`rgb(255,255,255)`;
+// })
+// document.querySelector(".resetBtn-black-lower").addEventListener
+// ("click", ()=>{
+//     document.querySelector(".slider-container-lower").style.background=`rgb(0,0,0)`;
+// })
 
